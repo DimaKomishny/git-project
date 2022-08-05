6089bdf22fd6ae86bfc1c1f4c95150bf6559ba36
dimak <dimakomishny@gmail.com>
2022-08-05 22:40:17 +0300

    added division operation

--- src/main/java/com/calc/App.java
+++ src/main/java/com/calc/App.java
@@ -5,15 +5,20 @@ public class App {
     public static void main(String[] args) {
         App calculator = new App();
         System.out.println(calculator.add(4, 2));
-        System.out.println(calculator.add(4, 2));
+        System.out.println(calculator.subtraction(4, 2));
+        System.out.println(calculator.division(5D, 3D));
     }
 
     public int add(int a, int b) {
         return a + b;
     }
 
-    public void subtraction(int a, int b) {
+    public int subtraction(int a, int b) {
         return a - b;
+    }
+
+    public double division(double a, double b) {
+        return a / b;
     }
 }
 
