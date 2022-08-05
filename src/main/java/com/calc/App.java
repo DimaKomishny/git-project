eee328f507ca76adc5591a4c26ac5122ea846284
dimak <dimakomishny@gmail.com>
2022-08-05 22:27:08 +0300

    added App.java and add method

---
+++ src/main/java/com/calc/App.java
@@ -0,0 +1,13 @@
+package com.calc;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println(new App().add(4, 2));
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
+
