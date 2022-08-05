a9973026f0e83639ef8f45723829f4a97aac48a1
dimak <dimakomishny@gmail.com>
2022-08-06 02:24:34 +0300

    added new sqrt operation

--- src/main/java/com/calc/App.java
+++ src/main/java/com/calc/App.java
@@ -7,6 +7,7 @@ public class App {
         System.out.println(calculator.add(4, 2));
         System.out.println(calculator.subtraction(4, 2));
         System.out.println(calculator.division(5D, 3D));
+        System.out.println(calculator.sqrt(3));
     }
 
     public int add(int a, int b) {
@@ -24,5 +25,17 @@ public class App {
     public double mult(double a, double b) {
         return a * b;
     }
-}
 
+    public double sqrt(int radical) {
+        if (radical == 0 || radical == 1) {
+            return radical;
+        }
+        double temp = 0;
+        double root = radical / 2;
+        while (root != temp) {
+            temp = root;
+            root = (radical / temp + temp) / 2;
+        }
+        return root;
+    }
+}
