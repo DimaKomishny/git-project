1bf6f5540369c034d655cc0228710db9695538b1
dimak <dimakomishny@gmail.com>
2022-08-05 22:30:55 +0300

    added subtraction operation

--- src/main/java/com/calc/App.java
+++ src/main/java/com/calc/App.java
@@ -3,11 +3,17 @@ package com.calc;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println(new App().add(4, 2));
+        App calculator = new App();
+        System.out.println(calculator.add(4, 2));
+        System.out.println(calculator.add(4, 2));
     }
 
     public int add(int a, int b) {
         return a + b;
+    }
+
+    public void subtraction(int a, int b) {
+        return a - b;
     }
 }
 
